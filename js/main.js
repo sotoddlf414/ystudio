ba94fae3a1e4debc2543887e73256180a30b95b7
kimjunsang <sotoddlf414@naver.com>
2020-06-10 20:44:35 +0900

    slick slider apply

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 
-$(function(){
 
-$('a').click(function(e){
+$(function(){
+// a preventDefault----------------------------------
+
+// $('a').click(function(e){
+//     e.preventDefault();
+// });    
+
+$(document).on('click', 'a[href="#"]', function(e){
     e.preventDefault();
-});    
+});
+
+// a preventDefault-end----------------------------------   
+
+
 
 
 // GNB-MENU
@@ -18,9 +28,42 @@ $('.gnbClose, section').click(function(){
 // GNB-MENU-end----------------------------------------------------------------------
 
 
+//Header-scroll-effect-------------------------------------------------------------------
+var Y_position = 0;
+
+$(window).scroll(function(){
+    Y_position = $(window).scrollTop();
+    if(Y_position>=130){
+        $('header').addClass('on');
+    }
+    else{
+        $('header').removeClass('on');
+    }
+})
+//Header-scroll-effect-end-------------------------------------------------------------------
+
+
+// scrolla plug-in apply--------------------------------------
+$('.animate').scrolla({
+      mobile: false
+    });
+// scrolla plug-in apply-end-------------------------------------
+
+
+// slick plug-in apply--------------------------------------
+
+$('.slide').slick({
+    arrow:true,
+    dots:false,
+    fade:true,
+    autoplay: false,
+    autoplaySpeed: 4000,
+  });
+
+// slick plug-in apply-end--------------------------------------
+
 // jquery-end-----------------------------
 });
-
 
 
 
